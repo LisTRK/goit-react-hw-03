3a7012ea1a5356e10ace1371c376435b31f9a007
Makhno <LisTRK54@gmail.com>
2025-05-04 20:13:45 +0300

    optimized fun contacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,43 @@
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
 
+// export default function ContactList({ contacts, onDeleteContact }) {
+//   const renderContacstList = contacts.map((contact) => {
+//     return (
+//       <li key={contact.id} className={css.card}>
+//         <Contact contact={contact} onDeleteContact={onDeleteContact} />
+//         <p>{contact.id}</p>
+//       </li>
+//     );
+//   });
+//   return (
+//     <ul className={css.list}>
+//       {renderContacstList.length !== 0 ? (
+//         renderContacstList
+//       ) : (
+//         <p className={css.empty}>Phonebook is empty</p>
+//       )}
+//     </ul>
+//   );
+// }
 export default function ContactList({ contacts, onDeleteContact }) {
-  const renderContacstList = contacts.map((contact) => {
-    return (
-      <li key={contact.id} className={css.card}>
-        <Contact contact={contact} onDeleteContact={onDeleteContact} />
-        <p>{contact.id}</p>
-      </li>
-    );
-  });
-  return (
-    <ul className={css.list}>
-      {renderContacstList.length !== 0 ? (
-        renderContacstList
-      ) : (
-        <p className={css.empty}>Phonebook is empty</p>
-      )}
-    </ul>
-  );
+  const renderContactsList = [
+    contacts.lenght !== 0
+      ? [
+          <ul className={css.list}>
+            {contacts.map((contact) => {
+              return (
+                <li key={contact.id} className={css.card}>
+                  <Contact
+                    contact={contact}
+                    onDeleteContact={onDeleteContact}
+                  />
+                </li>
+              );
+            })}
+          </ul>,
+        ]
+      : [<p className={css.empty}>Phonebook is empty</p>],
+  ];
+  return renderContactsList;
 }
